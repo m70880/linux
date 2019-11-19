cbe4d722154114bb9297e8c8c6bf5bc0969b052f
m70880 <51083575+m70880@users.noreply.github.com>
2019-11-19 17:13:23 +0800

    测试
    测试

--- tz/test.cpp
+++ tz/test.cpp
@@ -5,7 +5,7 @@ int main(int argc,char* argv[],char* env[]){
   (void)argc;
   for(int i=0;i<argc;i++){
     cout<<(argv[i])<<endl;
-  }
+  }//测试
   for(int i=0;env[i];i++){
     cout<<env[i]<<endl;
   }
