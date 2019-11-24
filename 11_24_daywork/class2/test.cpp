b5733f98a348ce837f9f3b139b734e9526436a42
m70880 <1017728152@qq.com>
2019-11-24 17:14:39 +0800

    update

--- 11_24_daywork/class2/test.cpp
+++ 11_24_daywork/class2/test.cpp
@@ -2,7 +2,7 @@
 #include <string>
 #include <vector>
 using namespace std;
-vector<string> split(string &str, char ch)
+vector<string> split(string &str, char ch)//将给定string分割成n个string类，ch作为分隔符
 {
 
     vector<string> res;
@@ -18,6 +18,22 @@ vector<string> split(string &str, char ch)
     }
     return res;
 }
+bool judgeRule(vector<string>& v){ 
+    //是否是5连
+    vector<string> vpoke = { "3","4","5","6","7","8", \
+        "9","10","J","Q","K", \
+            "A","2","joker","JOKER" };
+    if(v.size()>=5){
+        int flag=0;
+        size_t pos=vpoke.find(v[0]);
+        for(size_t i=0;i<v.size();i++){
+            
+        }
+        return flag;
+    }
+    return false;
+    
+};
 int main() {
     vector<string> vpoke = { "3","4","5","6","7","8", \
         "9","10","J","Q","K", \
@@ -27,14 +43,24 @@ int main() {
     vector<string> left;
     vector< string> right;
     getline(cin, tmp);
+    //将输入的牌分别存储在两个vector中
     auto result = split(tmp, ' ');
     for(size_t i=0;i<result.size();++i){
         if(result[i].find('-')!=-1){
-
+            left.push_back(result[i]);
+        }else {
+            auto result2=split(result[i],'-');
+            left.push_back(result2[0]);
+            right.push_back(result[2]);
+            i++;
+            for(;i<result.size();++i){
+                right.push_back(result[i]);
+            }
         }
     }
-
-
+    // das ----------------------------------------------------
+    
+    //判断各边是否符合要求
     return 0;
 
 }
