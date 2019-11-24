f15dc2d4b57fd6e78f5ab76be7568ae23e117936
m70880 <1017728152@qq.com>
2019-11-24 16:37:36 +0800

    update

--- 11_24_daywork/class2/test.cpp
+++ 11_24_daywork/class2/test.cpp
@@ -4,34 +4,38 @@
 using namespace std;
 vector<string> split(string &str, char ch)
 {
-        
-  vector<string> res;
-      str += ch;
-          size_t start = 0;
-              size_t last = str.find(ch);
-                      while (last < str.size())
-                      {
-                       if (start != last)
-                             res.push_back(str.substr(start, last - start));
-                        start = last + 1;
-                        last = str.find(ch, start);
-                                                          
-                      }
-                  return res;
+
+    vector<string> res;
+    str += ch;
+    size_t start = 0;
+    size_t last = str.find(ch);
+    while (last < str.size())
+    {
+        if (start != last)
+            res.push_back(str.substr(start, last - start));
+        start = last + 1;
+        last = str.find(ch, start);
+    }
+    return res;
+}
+int main() {
+    vector<string> vpoke = { "3","4","5","6","7","8", \
+        "9","10","J","Q","K", \
+            "A","2","joker","JOKER" };
+    string tmp;
+    vector<string> sum;
+    vector<string> left;
+    vector< string> right;
+    getline(cin, tmp);
+    auto result = split(tmp, ' ');
+    for(size_t i=0;i<result.size();++i){
+        if(result[i].find('-')!=-1){
+
+        }
+    }
+
+
+    return 0;
 
 }
-int main(){
-     vector<string> vpoke={"3","4","5","6","7","8",/
-             "9","10","J","Q","K",/
-             "A","2","joker","JOKER"};
-     string tmp; 
-     vector<string> sum;
-     vector<string> left;
-     vector< string> right;
-     getline(cin,tmp);
-     auto result = split(tmp.c_str()," -");
-     
-     
-     return 0;
 
-}
